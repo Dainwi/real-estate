24ce9d14ae394977f530cc9503dbcc5350ae202c
Dainwi <danwichoudhary@gmail.com>
2022-10-22 22:07:40 +0530

    Home Page is ready with Navbar

---
+++ src/data/SliderData.js
@@ -0,0 +1,78 @@
+import ImageOne from '../images/house-1.jpg'
+import ImageTwo from '../images/house-2.jpg'
+import ImageThree from '../images/house-3.jpg'
+import ImageFour from '../images/house-4.jpg'
+import ImageFive from '../images/house-5.jpg'
+import ImageSix from '../images/house-6.jpg'
+import ImageSeven from '../images/house-7.jpg'
+import ImageEight from '../images/house-8.jpg'
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Coast Rica",
+    price: "$2,860,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$8,420,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Huston, Texas",
+    price: "$6,660,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+
+export const SliderDataTwo = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$8,980,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageFive,
+    alt: "House",
+  },
+  {
+    title: "House in Las Vegas, Nevada",
+    price: "$3,460,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageSix,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$5,420,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageSeven,
+    alt: "House",
+  },
+  {
+    title: "House in Alajuela, Costa Rica",
+    price: "$4,280,000",
+    path: "/home",
+    lable: "View Home",
+    image: ImageEight,
+    alt: "House",
+  },
+];
