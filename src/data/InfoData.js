4de9e8615503ca06084c159aa8c8d285449c19cc
Dainwi <danwichoudhary@gmail.com>
2022-10-23 06:22:24 +0530

    InfoSection

---
+++ src/data/InfoData.js
@@ -0,0 +1,29 @@
+import ImageOne from "../images/house-2.jpg";
+import ImageTwo from "../images/house-8.jpg";
+
+export const InfoData = [
+  {
+    heading: "Explore our beautiful homes",
+    paragraphOne:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, facilis eveniet aperiam cum molestias maxime.",
+    paragraphTwo:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sapiente idsimilique aliquam quae, pariatur culpa suscipit nihil! Labore porro ipsa magnam officia quibusdam. Quis maxime repellat pariatur officiis voluptatibus.",
+    buttonLabels: "View Homes",
+    image: ImageOne,
+    reverse: false,
+    delay: 100,
+  },
+];
+export const InfoDataTwo = [
+  {
+    heading: "Modern Designs",
+    paragraphOne:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, facilis eveniet aperiam cum molestias maxime.",
+    paragraphTwo:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sapiente idsimilique aliquam quae, pariatur culpa suscipit nihil! Labore porro ipsa magnam officia quibusdam. Quis maxime repellat pariatur officiis voluptatibus.",
+    buttonLabels: "View Homes",
+    image: ImageTwo,
+    reverse: true,
+    delay: 300,
+  },
+];
